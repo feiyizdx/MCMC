faaa434e94be62603473ff84e527cdb3d8092ae2
Xin Bian <bianxin2006@gmail.com>
2016-10-21 17:24:18 -0400

    modify test

--- tests/test_markov.py
+++ tests/test_markov.py
@@ -14,18 +14,16 @@ import unittest
 
 from markov import markov
 
+class Testmarkov(unittest.markov):
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    def setUp(self):
+        pass
 
+    def tearDown(self):
+        pass
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument.
-    """
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    def test_000_something(self):
+        pass
+if __name__ == '__main__':
+    sys.exit(unittest.main())
+  # assert 'GitHub' in BeautifulSoup(response.content).title.string
