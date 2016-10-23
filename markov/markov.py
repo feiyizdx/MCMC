eb0db9da56c70f333ff641a45518d5ab9441b08b
Xin Bian <bianxin2006@gmail.com>
2016-10-22 20:19:12 -0400

    add p(j|i) p(i|j) calculation

--- markov/markov.py
+++ markov/markov.py
@@ -11,6 +11,7 @@ import sys
 m=5
 r=2.0
 istep=200
+T=10
 #generate 2D grid, dx=dy=1
 nxx, nyy = (m,m)
 x = np.linspace(0, m-1, nxx)
@@ -97,7 +98,14 @@ def cut_func():
              cut=True
       else:         
          cut=True  
-         
+
+#calculate stationary probality function
+def calc_weight():   
+    weight_1=r*FG.size(weight='weight')
+    weight_2=0.0
+    for i in range(m):
+        weight_2=weight_2+nx.shortest_path_length(FG,source=0,target=i, weight='weight')
+    return weight_1+weight_2        
          
 #print nx.edge_connectivity(FG, 4,0)
          
@@ -126,28 +134,62 @@ def check_nocut():
 #main loop
 
 i=0
+#initial theta
+tmp=calc_weight()
 while i<istep:
    #cannot cut case
- 
+   #propprob=proposal probaility q(j|i)
+   #propprob_2=q(i|j)
+  FT=nx.Graph(FG)
   if check_min()==True:
-    prob= 1.0/(m*(m-1)/2.0-1.0*FG.number_of_edges())
+    #store current graph as ft   
+    prop_prob = 1.0/(m*(m-1)/2.0-1.0*FG.number_of_edges())
     add_func()
+    if FG.number_of_edges()==m*(m-1)/2:
+        prop_prob_2 = 1.0/(FG.number_of_edges())
+    else:
+        prop_prob_2 = 0.5*1.0/(FG.number_of_edges())
+   
+    
+    
     
    #cannot add case
   elif FG.number_of_edges()==m*(m-1)/2:
-    prob= 1.0/(m*(m-1)/2.0)
+    prop_prob = 1.0/(m*(m-1)/2.0)
     cut_func()
+    if check_min()==True:       
+        prop_prob_2 = 1.0
+    else:
+        prop_prob_2 = 0.5*1.0
+        
     
    #choose add cut randomly
   else:
     a=random.randint(0,1)
     if a==1:
-        prob= 1.0/(m*(m-1)/2.0-1.0*FG.number_of_edges())
+        prop_prob = 0.5*1.0/(m*(m-1)/2.0-1.0*FG.number_of_edges())
         add_func()
+        if FG.number_of_edges()==m*(m-1)/2:
+            prop_prob_2 = 1.0/(FG.number_of_edges())
+        else:
+            prop_prob_2 = 0.5*1.0/(FG.number_of_edges())
     else:
         totaledge = FG.number_of_edges()
-        prob = 1.0/(totaledge-check_nocut())
+        prop_prob = 0.5*1.0/(totaledge-check_nocut())
         cut_func()
+        if check_min()==True:       
+            prop_prob_2 = 1.0/(m*(m-1)/2.0-FG.number_of_edges())
+        else:
+            prop_prob_2 = 0.5*1.0/(m*(m-1)/2.0-FG.number_of_edges())
+        
+  sta_prob = np.exp(-(calc_weight()-tmp)/T)
+  rand_uni=random.random()
+  if rand_uni<(min(sta_prob*prop_prob/prop_prob_2 ,1)):
+        pass
+  else:
+        FG=nx.Graph(FT)    
+  tmp=calc_weight()
+  
   i=i+1
   print FG.number_of_edges()
   #output test
@@ -161,19 +203,16 @@ for i in range(5):
     labels[i]=str(i)
     
 nx.draw(FG, with_labels=True)
+plt.show()
 
 edge_labels =dict([((u, v), d['label']) 
                    for u, v, d in FG.edges(data=True)])
 
 
+
 plt.show()
 
-#calculate stationary probality function
-weight_1=r*FG.size(weight='weight')
-weight_2=0.0
-#for i in range(m):
-#    weight_2=weight_2+nx.shortest_path_length(FG,source=0,target=i, weight='weight')
-#theta=weight_1+weight_2
+
 
 
 ####unit test
@@ -187,8 +226,8 @@ class Test_markov(unittest.TestCase):
     #test shortest path length
      def test_len(self):
          self.assertEqual(nx.shortest_path_length(FG,source=2,target=2, weight='weight'),0)
-     def test_propprob(self):
-         self.assertlessEqual(prob, 1.0)
+     def test_prop_prob(self):
+         self.assertlessEqual(prop_prob, 1.0)
          
         
 tests =  unittest.TestLoader().loadTestsFromTestCase(Test_markov)
