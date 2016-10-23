31da1184fc2f75b6369810993910ade4ce664396
Xin Bian <bianxin2006@gmail.com>
2016-10-22 21:17:31 -0400

    add matplot lib

--- markov/markov.py
+++ markov/markov.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # this is a code of MCMC
 import networkx as nx
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import unittest
