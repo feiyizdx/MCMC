8114eca279561c49046d7710245435a75205e4be
Xin Bian <bianxin2006@gmail.com>
2016-10-23 22:23:37 -0400

    sperate unit test

--- tests/test_markov.py
+++ tests/test_markov.py
@@ -16,14 +16,18 @@ from markov import markov
 
 class Testmarkov(unittest.TestCase):
 
-    def setUp(self):
-        pass
+  # test weights cal function
+     def test_weights(self):
+        self.assertEquals(cacl_weights(0.0, 1.0, 0.0, 2.0), 1.0)
+    #test distance calculation
+     def test_coordine(self):
+         self.assertEquals(cacl_weights(xv[xrange[1],yrange[1]], yv[xrange[1],yrange[1]],xv[xrange[1],yrange[1]], yv[xrange[1],yrange[1]]),0)
+    #test shortest path length
+     def test_len(self):
+         self.assertEqual(nx.shortest_path_length(FG,source=2,target=2, weight='weight'),0)
+  
 
-    def tearDown(self):
-        pass
 
-    def test_000_something(self):
-        pass
 if __name__ == '__main__':
     sys.exit(unittest.main())
   # assert 'GitHub' in BeautifulSoup(response.content).title.string
